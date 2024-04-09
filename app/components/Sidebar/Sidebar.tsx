fcb4ff24a0dd95f19c113904a7059cb6364e197e
Nico Diaz <nicodiazan@gmail.com>
2024-04-09 20:36:28 +0200

    Add delay so the whole thing doesn't render unless ready

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -14,7 +14,7 @@ const SidebarStyled = styled.nav`
 	position: relative;
 	width: ${({ theme }) => theme.sidebarWidth};
 	background-color: ${({ theme }) => theme.colorBg2};
-    border-right: 2px solid ${({ theme }) => theme.borderColor2};
+    border: 2px solid ${({ theme }) => theme.borderColor2};
 
     border-radius: 1rem;
 `;
