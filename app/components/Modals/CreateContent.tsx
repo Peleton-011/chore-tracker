64fc416ec46d99612ad24e62a1a2fe919da8f1da
Nico <nicodiazan@gmail.com>
2024-04-14 14:29:21 +0200

    Add basic submit handling

--- app/components/Modals/CreateContent.tsx
+++ app/components/Modals/CreateContent.tsx
@@ -31,83 +31,82 @@ function CreateContent() {
 		}
 	};
 
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+        
+	};
+
 	return (
-		<div>
-			<form>
-				<h1>Create a Task</h1>
-				<div className="input-control">
-					<label htmlFor="title">Title</label>
-					<input
-						name="title"
-						id="title"
-						type="text"
-						placeholder="Title"
-						value={title}
-						onChange={(e) => handleChange("title", e.target.value)}
-					/>
-				</div>
+		<form onSubmit={handleSubmit}>
+			<h1>Create a Task</h1>
+			<div className="input-control">
+				<label htmlFor="title">Title</label>
+				<input
+					name="title"
+					id="title"
+					type="text"
+					placeholder="Title"
+					value={title}
+					onChange={(e) => handleChange("title", e.target.value)}
+				/>
+			</div>
 
-				<div className="input-control">
-					<label htmlFor="description">Description</label>
-					<input
-						name="description"
-						id="description"
-						type="text"
-						placeholder="Description"
-						value={description}
-						onChange={(e) =>
-							handleChange("description", e.target.value)
-						}
-					/>
-				</div>
+			<div className="input-control">
+				<label htmlFor="description">Description</label>
+				<input
+					name="description"
+					id="description"
+					type="text"
+					placeholder="Description"
+					value={description}
+					onChange={(e) =>
+						handleChange("description", e.target.value)
+					}
+				/>
+			</div>
 
-				<div className="input-control">
-					<label htmlFor="date">Date</label>
-					<input
-						name="date"
-						id="date"
-						type="date"
-						placeholder="Date"
-						value={date}
-						onChange={(e) => handleChange("date", e.target.value)}
-					/>
-				</div>
+			<div className="input-control">
+				<label htmlFor="date">Date</label>
+				<input
+					name="date"
+					id="date"
+					type="date"
+					placeholder="Date"
+					value={date}
+					onChange={(e) => handleChange("date", e.target.value)}
+				/>
+			</div>
 
-				<div className="input-control">
-					<label htmlFor="completed">Completed</label>
-					<input
-						name="completed"
-						id="completed"
-						type="checkbox"
-						placeholder="Completed"
-						value={completed.toString()}
-						onChange={(e) =>
-							handleChange("completed", e.target.value)
-						}
-					/>
-				</div>
+			<div className="input-control">
+				<label htmlFor="completed">Completed</label>
+				<input
+					name="completed"
+					id="completed"
+					type="checkbox"
+					placeholder="Completed"
+					value={completed.toString()}
+					onChange={(e) => handleChange("completed", e.target.value)}
+				/>
+			</div>
 
-				<div className="input-control">
-					<label htmlFor="important">Important</label>
-					<input
-						name="important"
-						id="important"
-						type="checkbox"
-						placeholder="Important"
-						value={important.toString()}
-						onChange={(e) =>
-							handleChange("important", e.target.value)
-						}
-					/>
-				</div>
+			<div className="input-control">
+				<label htmlFor="important">Important</label>
+				<input
+					name="important"
+					id="important"
+					type="checkbox"
+					placeholder="Important"
+					value={important.toString()}
+					onChange={(e) => handleChange("important", e.target.value)}
+				/>
+			</div>
 
-				<div className="submit-btn">
-					<button type="submit">
-                        <span>Create</span>
-                    </button>
-				</div>
-			</form>
-		</div>
+			<div className="submit-btn">
+				<button type="submit">
+					<span>Create</span>
+				</button>
+			</div>
+		</form>
 	);
 }
 
