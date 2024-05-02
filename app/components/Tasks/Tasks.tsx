31abf18a8a546e4adc7379d94830e9796a190b22
Nico <nicodiazan@gmail.com>
2024-05-02 18:21:49 +0200

    Add some scaffolding to be used with future components

--- app/components/Tasks/Tasks.tsx
+++ app/components/Tasks/Tasks.tsx
@@ -8,112 +8,117 @@ import CreateContent from "../Modals/CreateContent";
 import TaskItem from "../TaskItem/TaskItem";
 
 interface Props {
-    title: string;
-    tasks: any[];
+	title: string;
+	tasks: any[];
 }
 
-
 function Tasks({ title, tasks }: Props) {
-	const { theme } = useGlobalState();
-    // console.log("TASKS: ", tasks)
+	const { theme, isLoading, openModal, modal } = useGlobalState();
+	// console.log("TASKS: ", tasks)
 	return (
 		<TaskStyled theme={theme}>
-			<CreateContent />
+			{modal && <Modal content={<CreateContent />} />}
+			<h1>{title}</h1>
 
-            {tasks.map((task: any) => (
-                <TaskItem key={task.id} {...task} />
-            ))}
-            <div className="create-task">
-                {plus} Add New Task
-            </div>
+			<button className="btn-rounded" onClick={openModal}>
+				{plus}
+			</button>
+
+			<div className="tasks grid">
+				{tasks.map((task: any) => (
+					<TaskItem key={task.id} {...task} />
+				))}
+
+				<button className="create-task" onClick={openModal}>{plus} Add New Task</button>
+			</div>
 		</TaskStyled>
 	);
 }
 
 const TaskStyled = styled.main`
-  position: relative;
-  padding: 2rem;
-  width: 100%;
-  background-color: ${(props) => props.theme.colorBg2};
-  border: 2px solid ${(props) => props.theme.borderColor2};
-  border-radius: 1rem;
-  height: 100%;
+	position: relative;
+	padding: 2rem;
+	width: 100%;
+	background-color: ${(props) => props.theme.colorBg2};
+	border: 2px solid ${(props) => props.theme.borderColor2};
+	border-radius: 1rem;
+	height: 100%;
 
-  overflow-y: auto;
+	overflow-y: auto;
 
-  &::-webkit-scrollbar {
-    width: 0.5rem;
-  }
+	&::-webkit-scrollbar {
+		width: 0.5rem;
+	}
 
-  .btn-rounded {
-    position: fixed;
-    top: 4.9rem;
-    right: 5.1rem;
-    width: 3rem;
-    height: 3rem;
-    border-radius: 50%;
+	.btn-rounded {
+		position: fixed;
+		top: 4.9rem;
+		right: 5.1rem;
+		width: 3rem;
+		height: 3rem;
+		border-radius: 50%;
 
-    background-color: ${(props) => props.theme.colorBg};
-    border: 2px solid ${(props) => props.theme.borderColor2};
-    box-shadow: 0 3px 15px rgba(0, 0, 0, 0.3);
-    color: ${(props) => props.theme.colorGrey2};
-    font-size: 1.4rem;
+		background-color: ${(props) => props.theme.colorBg};
+		border: 2px solid ${(props) => props.theme.borderColor2};
+		box-shadow: 0 3px 15px rgba(0, 0, 0, 0.3);
+		color: ${(props) => props.theme.colorGrey2};
+		font-size: 1.4rem;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 
-    @media screen and (max-width: 768px) {
-      top: 3rem;
-      right: 3.5rem;
-    }
-  }
+		@media screen and (max-width: 768px) {
+			top: 3rem;
+			right: 3.5rem;
+		}
+	}
 
-  .tasks {
-    margin: 2rem 0;
-  }
+	.tasks {
+		margin: 2rem 0;
+	}
 
-  > h1 {
-    font-size: clamp(1.5rem, 2vw, 2rem);
-    font-weight: 800;
-    position: relative;
+	> h1 {
+		font-size: clamp(1.5rem, 2vw, 2rem);
+		font-weight: 800;
+		position: relative;
 
-    &::after {
-      content: "";
-      position: absolute;
-      bottom: -0.5rem;
-      left: 0;
-      width: 3rem;
-      height: 0.2rem;
-      background-color: ${(props) => props.theme.colorPrimaryGreen};
-      border-radius: 0.5rem;
-    }
-  }
+		&::after {
+			content: "";
+			position: absolute;
+			bottom: -0.5rem;
+			left: 0;
+			width: 3rem;
+			height: 0.2rem;
+			background-color: ${(props) => props.theme.colorPrimaryGreen};
+			border-radius: 0.5rem;
+		}
+	}
 
-  .create-task {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 0.5rem;
+	.create-task {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 0.5rem;
 
-    height: 16rem;
-    color: ${(props) => props.theme.colorGrey2};
-    font-weight: 600;
-    cursor: pointer;
-    border-radius: 1rem;
-    border: 3px dashed ${(props) => props.theme.colorGrey5};
-    transition: all 0.3s ease;
+		height: 16rem;
+		color: ${(props) => props.theme.colorGrey2};
+		font-weight: 600;
+		cursor: pointer;
+		border-radius: 1rem;
+		border: 3px dashed ${(props) => props.theme.colorGrey5};
+		transition: all 0.3s ease;
 
-    i {
-      font-size: 1.5rem;
-      margin-right: 0.2rem;
-    }
+		i {
+			font-size: 1.5rem;
+			margin-right: 0.2rem;
+		}
 
-    &:hover {
-      background-color: ${(props) => props.theme.colorGrey5};
-      color: ${(props) => props.theme.colorGrey0};
-    }
-  }
+		&:hover {
+			background-color: ${(props) => props.theme.colorGrey5};
+			color: ${(props) => props.theme.colorGrey0};
+		}
+	}
 `;
 
 export default Tasks;
