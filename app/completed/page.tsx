fcdcdcb7b90e13945af52609cf384ab18cbd7925
Nico <nicodiazan@gmail.com>
2024-05-11 14:10:22 +0200

    Implement the "Completed" page

--- app/completed/page.tsx
+++ app/completed/page.tsx
@@ -1,11 +1,11 @@
-"use client"
-
-import React from 'react'
+"use client";
+import React from "react";
+import { useGlobalState } from "@/app/context/globalProvider";
+import Tasks from "../components/Tasks/Tasks";
 
 function page() {
-  return (
-    <div>completed</div>
-  )
+	const { completedTasks } = useGlobalState();
+	return <Tasks title="Completed Tasks" tasks={completedTasks} />;
 }
 
-export default page
+export default page;
