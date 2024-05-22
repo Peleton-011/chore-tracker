a6f259a5cb38b7fe6928d153e4f51772e62b88ba
Nico Diaz <nicodiazan@gmail.com>
2024-05-22 16:36:52 +0200

    Refactor mongoose connection code to betteradapt to new requiremens

--- app/utils/connect.ts
+++ app/utils/connect.ts
@@ -1,25 +1,26 @@
 import mongoose from "mongoose";
 
-console.log(process.env)
+console.log(process.env);
 
-const MONGODB_URI  = process.env.MONGODB_URI || "mongodb+srv://nico:uYF1MlqJmvWlRxck@mytasks.7l9kmdf.mongodb.net/MyTasks" ;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
 	throw new Error("MongoDB connection URI is not provided");
 }
 
-const mongooseConnection = () => mongoose
-	.connect(
-		MONGODB_URI
-		//     ,{
-		//   useNewUrlParser: true,
-		//   useUnifiedTopology: true,
-		// }
-	)
-	.then(() => console.log("Connected to MongoDB"))
-	.catch((error: unknown) => {
-		console.error("MongoDB connection error:", error);
-		throw error;
-	});
+const mongooseConnection = async () => {
+	if (mongoose.connections[0].readyState) {
+		// Use current db connection
+		return;
+	}
+
+	return await mongoose
+		.connect(MONGODB_URI)
+		.then(() => console.log("Connected to MongoDB"))
+		.catch((error: unknown) => {
+			console.error("MongoDB connection error:", error);
+			throw error;
+		});
+};
 
 export default mongooseConnection;
