943f0c255b12cf30fdac2f48bdbaf840ddd50a6e
Nico Diaz <nicodiazan@gmail.com>
2024-06-14 18:12:59 +0200

    Finish up initial restyling

--- app/context/globalProvider.js
+++ app/context/globalProvider.js
@@ -10,7 +10,7 @@ export const GlobalUpdateContext = createContext();
 export const GlobalProvider = ({ children }) => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [modal, setModal] = useState(false);
-	const [collapsed, setCollapsed] = useState(false);
+	const [collapsed, setCollapsed] = useState(true);
 
 	const [tasks, setTasks] = useState([]);
 
