845e1789e0d66341c78859dfd218263c7fe5ea99
Nico Diaz <nicodiazan@gmail.com>
2024-07-05 20:02:26 +0200

    Minor tweaks to better fit the vision for the tasks component

--- app/components/Tasks/Tasks.tsx
+++ app/components/Tasks/Tasks.tsx
@@ -20,6 +20,7 @@ function Tasks({ lists }: Props) {
 	const { isLoading, modal, editedTask, createTask, editTask } =
 		useGlobalState();
 
+	lists = lists.filter((list) => list.tasks.length > 0);
 	// console.log("TASKS: ", tasks)
 	return (
 		<div className="tasks-container">
@@ -27,30 +28,37 @@ function Tasks({ lists }: Props) {
 
 			{modal && <Modal content={<CreateContent task={editedTask} />} />}
 
-			{lists.map(({ title, tasks }) => {
+			{lists.map(({ title, tasks }, index) => {
 				return (
-					tasks.length > 0 && (
-						<>
-							<h1>{title}</h1>
+					<>
+						<h1>{title}</h1>
 
-							<div className="tasks">
-								{tasks &&
-									tasks.map((task: any) => (
-										<TaskItem
-											key={task._id}
-											id={task._id}
-											{...task}
-											editTask={editTask}
-										/>
-									))}
-							</div>
-						</>
-					)
+						<div className="tasks">
+							{tasks &&
+								tasks.map((task: any) => (
+									<TaskItem
+										key={task._id}
+										id={task._id}
+										{...task}
+										editTask={editTask}
+									/>
+								))}
+
+							{
+								// If its the last element in the list, add the create task button
+								index === lists.length - 1 && (
+									<button
+										className="create-task"
+										onClick={createTask}
+									>
+										{plus} Add New Task
+									</button>
+								)
+							}
+						</div>
+					</>
 				);
 			})}
-			<button className="create-task" onClick={createTask}>
-				{plus} Add New Task
-			</button>
 		</div>
 	);
 }
