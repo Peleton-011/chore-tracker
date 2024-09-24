95d865c70d287d6b9b1989678692c84a229b29e6
Nico Diaz <nicodiazan@gmail.com>
2024-09-24 18:19:25 +0200

    Update household page to better fit the new model

--- app/household/page.tsx
+++ app/household/page.tsx
@@ -3,40 +3,20 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useGlobalState } from "@/app/context/globalProvider";
 import axios from "axios";
-import Tasks from "../components/Tasks/Tasks";
+import { edit } from "../utils/Icons";
 
 interface Household {
 	_id: string;
 	name: string;
+	members: string[];
+	tasks: any[];
+	recurringTasks: any[];
 }
 
 function page() {
-	const { households, fetchHouseholds } = useGlobalState();
-	const [newHouseholdName, setNewHouseholdName] = useState("");
+	const { households, editHousehold, deleteHousehold, createHousehold } =
+		useGlobalState();
 	const [error, setError] = useState<string>("");
-
-	const createHousehold = async () => {
-		if (!newHouseholdName) {
-			alert("Please enter a household name");
-			return;
-		}
-
-		try {
-			const response = await axios.post("/api/household", {
-				name: newHouseholdName,
-			});
-
-			if (response.status === 201 || response.status === 200) {
-				fetchHouseholds(); // Refresh the households list
-				setNewHouseholdName("");
-			} else {
-				alert(response.data.error);
-				console.error(JSON.stringify(response, null, 2));
-			}
-		} catch (err) {
-			alert("Failed to create household");
-		}
-	};
 
 	const generateInviteLink = async (householdId: string) => {
 		try {
@@ -44,15 +24,12 @@ function page() {
 				`/api/household/${householdId}/invite`
 			);
 
-			if (response.status === 201) {
-				alert(`Invite link: ${response.data.inviteLink}`);
-			} else {
-				alert(response.data.error);
-			}
+			alert(`Invite link: ${response.data.inviteLink}`);
 		} catch (err) {
-			alert("Failed to generate invite link");
+			alert("Failed to generate invite link: " + err);
 		}
 	};
+
 	return (
 		<div>
 			<h1>Your Households</h1>
@@ -66,18 +43,27 @@ function page() {
 						>
 							Generate Invite Link
 						</button>
+
+						<button
+							className="edit"
+							onClick={() => {
+								console.log(households);
+								editHousehold({
+									...household,
+									id: household._id,
+								});
+							}}
+						>
+							{edit}
+						</button>
+
+						<button onClick={() => deleteHousehold(household._id)}>
+							Delete
+						</button>
 					</li>
 				))}
 			</ul>
-
-			<h2>Create New Household</h2>
-			<input
-				type="text"
-				value={newHouseholdName}
-				onChange={(e) => setNewHouseholdName(e.target.value)}
-				placeholder="Household Name"
-			/>
-			<button onClick={createHousehold}>Create</button>
+			<button onClick={createHousehold}>Create Household</button>
 		</div>
 	);
 }
