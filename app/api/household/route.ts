66acfe7876caad38b671bbe9a6e251bad6a1eaf2
Nico Diaz <nicodiazan@gmail.com>
2024-09-24 18:17:42 +0200

    Update household api to fit the new model

--- app/api/household/route.ts
+++ app/api/household/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import mongooseConnection from "@/app/utils/connect";
 import { Household, User } from "@/models/index";
 import { auth } from "@clerk/nextjs";
+import { ObjectId } from "mongoose";
 
 export async function GET(req: NextRequest, res: NextResponse) {
 	try {
@@ -42,5 +43,69 @@ export async function POST(req: NextRequest, res: NextResponse) {
 	} catch (error) {
 		console.error(error);
 		return NextResponse.json({ error: "Server error", status: 500 });
+	}
+}
+
+export async function PUT(req: NextRequest, res: NextResponse) {
+	try {
+		const { userId } = auth();
+		if (!userId) {
+			return NextResponse.json({ error: "Unauthorized", status: 401 });
+		}
+
+		const updates = await req.json();
+		const { id, ...updateFields } = updates;
+
+		if (!id) {
+			return NextResponse.json({
+				error: "Missing household ID",
+				status: 400,
+			});
+		}
+
+		const household = await Household.findById(id);
+
+		console.log(household);
+		if (!household) {
+			return NextResponse.json({
+				error: "Household not found",
+				status: 404,
+			});
+		}
+
+		const user = await User.findOne({ userId });
+
+		if (!user) {
+			return NextResponse.json({
+				error: "User not found",
+				status: 404,
+			});
+		}
+
+		if (
+			!household.members
+				.map((id: ObjectId) => id.toString())
+				.includes(user._id.toString())
+		) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		console.log(updateFields);
+
+		Object.keys(updateFields).forEach((key) => {
+			household[key] = updateFields[key];
+		});
+
+        console.log
+
+		await household.save();
+
+		return NextResponse.json(household);
+	} catch (error) {
+		console.log("ERROR UPDATING HOUSEHOLD: ", error);
+		return NextResponse.json({
+			error: "Error updating household",
+			status: 500,
+		});
 	}
 }
