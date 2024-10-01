34202922bfd35bca58670e89fce189006867d03b
Nico Diaz <nicodiazan@gmail.com>
2024-10-01 09:07:52 +0200

    Move all database changes to one transaction

--- app/api/invite/[token]/route.ts
+++ app/api/invite/[token]/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import mongooseConnection from "@/app/utils/connect";
 import { Invite, Household, User } from "@/models/index";
 import { auth } from "@clerk/nextjs";
+import mongoose from "mongoose";
 
 export async function GET(
 	req: NextRequest,
@@ -55,6 +56,10 @@ export async function POST(
 	req: NextRequest,
 	{ params }: { params: { token: string } }
 ) {
+	// Start a session
+	const session = await mongoose.startSession();
+	session.startTransaction();
+
 	try {
 		const { token } = params;
 		const { userId } = auth();
@@ -67,8 +72,10 @@ export async function POST(
 			});
 		}
 
-		const invite = await Invite.findOne({ token });
+		const invite = await Invite.findOne({ token }).session(session);
 		if (!invite) {
+			await session.abortTransaction();
+			session.endSession();
 			return NextResponse.json({
 				error: "Invalid invite token",
 				status: 404,
@@ -76,31 +83,40 @@ export async function POST(
 		}
 
 		if (invite.expiresAt < new Date()) {
+			await session.abortTransaction();
+			session.endSession();
 			return NextResponse.json({
 				error: "Invite link has expired",
 				status: 400,
 			});
 		}
         
-		const household = await Household.findById(invite.household);
-        const user = await User.findOne({userId});
+		const household = await Household.findById(invite.household).session(session);
+        const user = await User.findOne({userId}).session(session);
 		if (!household) {
+			await session.abortTransaction();
+			session.endSession();
 			return NextResponse.json({
 				error: "Household not found",
 				status: 404,
 			});
 		}
 
-
+		// Update household members if user is not already a member
 		if (!household.members.includes(user._id)) {
 			household.members.push(user._id);
-			await household.save();
+			await household.save({ session });
 		}
 
+		// Update user's households if the household is not already included
 		if (!user.households.includes(household._id)) {
 			user.households.push(household._id);
-			await user.save();
+			await user.save({ session });
 		}
+
+		// Commit the transaction
+		await session.commitTransaction();
+		session.endSession();
 
 		return NextResponse.json({
 			message: "User added to household",
@@ -108,6 +124,11 @@ export async function POST(
 		});
 	} catch (error) {
 		console.error("Error adding user to household:", error);
+
+		// Abort the transaction if there is an error
+		await session.abortTransaction();
+		session.endSession();
+
 		return NextResponse.json({ error: "Server error", status: 500 });
 	}
 }
