1065fa41d9f42a636e44130fb40bd2f4117a83f6
Nico Diaz <nicodiazan@gmail.com>
2024-10-16 15:03:45 +0200

    Change the invite page to be more descriptive

--- app/invite/[token]/page.tsx
+++ app/invite/[token]/page.tsx
@@ -1,6 +1,6 @@
 "use client"; // This ensures the component is client-side
 import { auth } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { useGlobalState } from "@/app/context/globalProvider";
 
 interface Household {
@@ -16,38 +16,29 @@ interface InviteResponse {
 
 // You receive params directly in the component when using the new app directory
 export default function page({ params }: { params: { token: string } }) {
-	const { joinHousehold } = useGlobalState();
+	const { joinHousehold, fetchHouseholdFromToken } = useGlobalState();
 	const { token } = params; // Use params to get the token
-	// const [household, setHousehold] = useState<Household | null>(null);
-	// const [error, setError] = useState<string>("");
 
-	// useEffect(() => {
-	// 	if (token) {
-	//         console.log(typeof token)
-	// 		fetch(`/api/invite/${token}`)
-	// 			.then((res) => res.json())
-	// 			.then((data: InviteResponse) => {
-	// 				if (data.error) {
-	// 					setError(data.error);
-	// 				} else {
-	// 					setHousehold(data.household);
-	// 				}
-	// 			})
-	// 			.catch((err) => setError("Failed to fetch invite details"));
-	// 	}
-	// }, [token]);
+	const [household, setHousehold] = useState<Household>();
 
-	// if (error) {
-	// 	return <div>{error}</div>;
-	// }
+	//Fetch household from token (async)
+	useEffect(() => {
+		const fetchHousehold = async () => {
+			const fetchedHousehold = await fetchHouseholdFromToken(token);
+			setHousehold(fetchedHousehold);
+		};
+		fetchHousehold();
+	}, []);
 
-	// if (!household) {
-	// 	return <div>Loading...</div>;
-	// }
+	if (!household) {
+		return <div>Loading...</div>;
+	}
 
 	return (
 		<div>
-			{/* <h1>Join Household: {household.name}</h1> */}
+			<h1>
+				You've been invited to join <i><u>{household.name}</u></i>!
+			</h1>
 			<button onClick={() => joinHousehold(token)}>Join Household</button>
 		</div>
 	);
