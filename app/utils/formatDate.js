f95aed695e3ecc9da81347515f1f9bc9c26b4d65
Nico Diaz <nicodiazan@gmail.com>
2024-11-13 12:06:55 +0100

    Add some new and useful utility datetime formatting functions

--- app/utils/formatDate.js
+++ app/utils/formatDate.js
@@ -3,11 +3,21 @@ import { format } from "date-fns";
 const formatDate = (date) => {
 	let out;
 	try {
-		out = format(date, "EEEE d/M yy");
+		out = format(date, "EEEE d/M/yy");
 	} catch (error) {
 		out = "Error displaying date";
 	}
 	return out;
 };
 
+function formatTime(date, use12HourFormat = false) {
+	return use12HourFormat ? format(date, "hh:mm a") : format(date, "HH:mm");
+}
+
+function formatDateTime(date, use12HourFormat = false) {
+	return (
+		format(date, "EE d/M/yy") + " , " + formatTime(date, use12HourFormat)
+	);
+}
 export default formatDate;
+export { formatTime, formatDateTime };
