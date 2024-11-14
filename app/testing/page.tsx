9a59dbfd0402bb10a0307a7022d84b9d3823e210
Nico Diaz <nicodiazan@gmail.com>
2024-11-14 23:04:14 +0100

    Add some of these recently added features to the testing page

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,12 +1,18 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import AutonomousModal from "../components/Modals/AutonomousModal";
+import ImagePicker from "../components/ImagePicker/ImagePicker";
+import HouseIcon from "@/public/icons/HouseIcon";
+import SchoolIcon from "@/public/icons/SchoolIcon";
+import OfficeIcon from "@/public/icons/OfficeIcon";
 
 const ExamplePage: React.FC = () => {
 	const [isModalOpen, setModalOpen] = useState(false);
 
 	const openModal = () => setModalOpen(true);
 	const closeModal = () => setModalOpen(false);
+
+	const icons = [<HouseIcon />, <SchoolIcon />, <OfficeIcon />];
 
 	const ContentComponent = ({ closeModal }: { closeModal: () => void }) => (
 		<div>
@@ -21,6 +27,9 @@ const ExamplePage: React.FC = () => {
 			<h1>Example Page</h1>
 			<button onClick={openModal}>Open Modal</button>
 
+			<h3>Choose an icon</h3>
+			<ImagePicker icons={icons} />
+			<HouseIcon />
 			<AutonomousModal
 				isOpen={isModalOpen}
 				onClose={closeModal}
