f1aa2f8d164ce4c7ab7db985c1c2b77b1ee56de0
Nico Diaz <nicodiazan@gmail.com>
2024-11-23 15:32:34 +0100

    Update some models and make the Notification model to better suit new functionality

--- models/index.ts
+++ models/index.ts
@@ -4,12 +4,6 @@ import Household from "./Household";
 import TaskTrade from "./TaskTrade";
 import TaskRotation from "./TaskRotation";
 import Invite from "./Invite";
+import Notification from "./Notification";
 
-export {
-	Task,
-	User,
-	Household,
-	TaskTrade,
-	TaskRotation,
-	Invite,
-};
+export { Task, User, Household, TaskTrade, TaskRotation, Invite, Notification };
