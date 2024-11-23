3cfb91340ae1d934bb0ae4c369cbf3a760e7aedc
Nico Diaz <nicodiazan@gmail.com>
2024-11-23 15:29:04 +0100

    Add the ReminderSelector to the /testing page

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 import React, { useState } from "react";
 
+import ReminderSelector from "../components/ReminderSelector/ReminderSelector";
 
 const ExamplePage: React.FC = () => {
-
-
+	const [reminders, setRemindeers] = useState<any[]>([]);
 
 	return (
 		<div>
 			<h1>Testing Page</h1>
 
-			
+			<ReminderSelector
+				reminders={reminders}
+				setReminders={setRemindeers}
+			/>
 		</div>
 	);
 };
