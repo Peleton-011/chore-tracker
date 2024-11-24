3be2a5136a779cb35aee423dfecaaf92f1261306
Nico Diaz <nicodiazan@gmail.com>
2024-11-24 08:52:58 +0100

    Put the reminder selector inside an AutonomousModal in the /testing page

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -2,18 +2,29 @@
 import React, { useState } from "react";
 
 import ReminderSelector from "../components/ReminderSelector/ReminderSelector";
+import AutonomousModal from "../components/Modals/AutonomousModal";
 
 const ExamplePage: React.FC = () => {
 	const [reminders, setRemindeers] = useState<any[]>([]);
+	const [isReminderModalOpen, setReminderModalOpen] = useState(false);
 
 	return (
 		<div>
 			<h1>Testing Page</h1>
 
-			<ReminderSelector
-				reminders={reminders}
-				setReminders={setRemindeers}
-			/>
+			<button onClick={() => setReminderModalOpen(true)}>
+				Set Reminders
+			</button>
+
+			<AutonomousModal
+				isOpen={isReminderModalOpen}
+				onClose={() => setReminderModalOpen(false)}
+			>
+				<ReminderSelector
+					reminders={reminders}
+					setReminders={setRemindeers}
+				/>
+			</AutonomousModal>
 		</div>
 	);
 };
