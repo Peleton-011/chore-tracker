dfa52709fc76cee2887d9d77f2fa94ee5a4a8443
Nico Diaz <nicodiazan@gmail.com>
2024-12-26 19:21:40 +0100

    Move main page over to use the new utility

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,12 @@
 "use client";
 import Tasks from "./components/Tasks/Tasks";
 import { useGlobalState } from "@/app/context/globalProvider";
+import dateTaskUtils from "./utils/dateTaskUtils";
 
 export default function Home() {
-	const { filteredTasks } = useGlobalState();
+	const { tasks } = useGlobalState();
 
-	const taskLists = [
-		{
-			title: "Overdue",
-			tasks: filteredTasks.overdue,
-		},
-		{
-			title: "Today",
-			tasks: filteredTasks.today,
-		},
-		{
-			title: "Later this week",
-			tasks: filteredTasks.laterThisWeek,
-		},
-		{
-			title: "Later this month",
-			tasks: filteredTasks.laterThisMonth,
-		},
-		{
-			title: "Someday",
-			tasks: filteredTasks.someday,
-		},
-	];
+	const taskLists = dateTaskUtils.filterAll(tasks);
 
     return (
 		<>
