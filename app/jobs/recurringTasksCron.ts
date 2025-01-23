a01c4f1b44a56180226743b626b0b6165ca9bd29
Nico Diaz <nicodiazan@gmail.com>
2025-01-23 13:23:12 +0100

    Disable cron job in lieu of a more powerful API

--- app/jobs/recurringTasksCron.ts
+++ app/jobs/recurringTasksCron.ts
@@ -1,19 +1,24 @@
-import cron from 'node-cron';
-import RecurringTaskDefinition from '@/models/RecurringTaskDefinition';
-import generatePlaceholders from '../utils/generatePlaceholders';
+import cron from "node-cron";
+import RecurringTaskDefinition from "@/models/RecurringTaskDefinition";
+import generatePlaceholders from "../utils/generatePlaceholders";
 
-cron.schedule('0 0 * * *', async () => { // Runs daily at midnight
-  console.log('Running placeholder task generation...');
-  const definitions = await RecurringTaskDefinition.find();
+cron.schedule("0 0 * * *", async () => {
+	// Runs daily at midnight
+	return;
+	console.log("Running placeholder task generation...");
+	const definitions = await RecurringTaskDefinition.find();
 
-  for (const definition of definitions) {
-    try {
-      await generatePlaceholders(definition._id);
-      console.log(`Updated placeholders for definition ${definition._id}`);
-    } catch (err) {
-      console.error(`Failed to update placeholders for definition ${definition._id}:`, err);
-    }
-  }
+	for (const definition of definitions) {
+		try {
+			await generatePlaceholders(definition._id);
+			console.log(
+				`Updated placeholders for definition ${definition._id}`
+			);
+		} catch (err) {
+			console.error(
+				`Failed to update placeholders for definition ${definition._id}:`,
+				err
+			);
+		}
+	}
 });
-
-
