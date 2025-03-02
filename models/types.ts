cae19c973680c71647967013d14c4a44696a29a6
Zhuo <nicodiazan@gmail.com>
2025-03-02 18:32:28 +0100

    Add some more types

--- models/types.ts
+++ models/types.ts
@@ -1,8 +1,36 @@
+
+export interface Task {
+    _id: string;
+    title: string;
+    description?: string;
+    date: Date;
+    isCompleted: boolean;
+    isImportant: boolean;
+    user: string; // Reference to the User model
+    household?: string; // Reference to the Household model
+    recurringTaskDefinition?: string; // Reference to the RecurringTaskDefinition model
+    isPlaceholder: boolean;
+    reminders: Reminder[];
+    createdAt: Date; // Timestamps fields
+    updatedAt: Date;
+  }
+  
+  export interface Reminder {
+    type: "before" | "after";
+    offsetMinutes: number;
+    notified: boolean;
+  }
+
 export interface Household {
 	_id: string;
 	name: string;
 	members: string[];
-    description: string;
+	description?: string;
 	tasks: any[];
 	recurringTasks: any[];
+}
+
+export interface TaskList {
+	title: string;
+	tasks: any[];
 }
