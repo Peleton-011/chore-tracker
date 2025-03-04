989c580afe0ad2e2c2537d1f00eb51ca1791be2d
Zhuo <nicodiazan@gmail.com>
2025-03-04 08:58:10 +0100

    Start adding a different CreateTask to the specific household page

--- app/households/[id]/page.tsx
+++ app/households/[id]/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { useGlobalState } from "@/app/context/globalProvider";
-import { edit } from "../../utils/Icons";
 import Tasks from "@/app/components/Tasks/Tasks";
 import axios from "axios";
 import CopyShareButton from "@/app/components/CopyShareButton/CopyShareButton";
@@ -9,35 +7,46 @@ import UserList from "@/app/components/UserList/UserList";
 import Description from "@/app/components/Description/Desscription";
 import dateTaskUtils from "@/app/utils/dateTaskUtils";
 
-import { TaskList, Household, Task } from "@/models/types";
+import { Task, TaskList, DEFAULT_TASK as DEF } from "@/models/types";
 
 import {
 	fetchHouseholdTasks,
 	generateInviteLink,
 } from "@/app/utils/households";
 import toast from "react-hot-toast";
+import AutonomousModal from "@/app/components/Modals/AutonomousModal";
+import CreateTask from "@/app/components/Forms/CreateTask";
+import { useGlobalState } from "@/app/context/globalProvider";
 
 function page({ params }: { params: { id: string } }) {
-	const [error, setError] = useState<string>("");
 	const { id } = params; // Use params to get the token
+	const [error, setError] = useState<string>("");
 	const [household, setHousehold] = useState<any>({ tasks: [], members: [] });
 	const [link, setLink] = useState("");
 	const [taskLists, setTaskLists] = useState<TaskList[]>([]);
+
+    const DEFAULT_TASK = {...DEF, household: id};
+
+	const [taskModal, setTaskModal] = useState(false);
+	const [editingTask, setEditingTask] = useState<Task>({...DEFAULT_TASK});
+
+	const { updateTask, createTask } = useGlobalState();
+
 
 	// Fetch & filter tasks
 	useEffect(() => {
 		const fetch = async () => {
 			try {
 				const response = await fetchHouseholdTasks(id);
-                setTaskLists(dateTaskUtils.filterAll(response));
+				setTaskLists(dateTaskUtils.filterAll(response));
 			} catch (error: any) {
 				toast.error(
 					"Something went wrong fetching tasks: " + error.message
 				);
 			}
 		};
-        fetch()
-	}, [household.tasks]);
+		fetch();
+	}, [id]);
 
 	// Fetch household from /api/households/[id]
 	useEffect(() => {
@@ -110,6 +119,26 @@ function page({ params }: { params: { id: string } }) {
 				<h3>Household Tasks</h3>
 				<Tasks lists={taskLists} />
 			</div>
+
+			{/* For household based/ multi user tasks */}
+			<AutonomousModal
+				isOpen={taskModal}
+				onClose={() => setTaskModal(false)}
+			>
+				<CreateTask
+					task={editingTask || {...DEFAULT_TASK}}
+					updateTask={(task) => {
+						updateTask({ ...task, household: id });
+						setEditingTask({...DEFAULT_TASK});
+						setTaskModal(false);
+					}}
+					createTask={(task) => {
+                        console.log(task)
+						createTask({ ...task, household: id });
+						setTaskModal(false);
+					}}
+				></CreateTask>
+			</AutonomousModal>
 		</div>
 	);
 }
