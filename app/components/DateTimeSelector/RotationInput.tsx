7cc0f8123b91f4cd19f3afe3b3fbd642c27b49bc
Zhuo <nicodiazan@gmail.com>
2025-03-07 10:11:48 +0100

    Make the RotationInput somewhat work

--- app/components/DateTimeSelector/RotationInput.tsx
+++ app/components/DateTimeSelector/RotationInput.tsx
@@ -1,5 +1,6 @@
 import { User } from "@/models/types";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { trash } from "@/app/utils/Icons";
 
 interface RotationInputProps {
 	members: User[];
@@ -16,6 +17,22 @@ const RotationInput = ({
 }: RotationInputProps) => {
 	const [schedule, setSchedule] = useState(argSchedule);
 
+	const randomSchedule = (length: number) => {
+		const newSchedule = [];
+		for (let i = 0; i < length; i++) {
+			newSchedule.push(
+				new Array(members.length)
+					.fill(false)
+					.map(() => Math.random() < 0.5)
+			);
+		}
+		return newSchedule;
+	};
+
+	useEffect(() => {
+		setSchedule(randomSchedule(3));
+	}, []);
+
 	const addRow = () => {
 		setSchedule([...schedule, new Array(members.length).fill(false)]);
 	};
@@ -28,22 +45,62 @@ const RotationInput = ({
 
 	// Grid with members and checkboxes
 	return (
-		<div>
-			{members.map((member) => (
-				<div key={member._id} className="user-card">
-					<img
-						src={member.avatar}
-						alt={`${member.name}'s avatar`}
-						className="avatar"
-						style={{
-							width: "50px",
-							height: "50px",
-							borderRadius: "50%",
-						}}
-					/>
-					<p>{member.name}</p>
+		<div
+			className="grid"
+			style={{
+				gridTemplateColumns: `repeat(${
+					members.length + 2
+				}, 1fr)`,
+			}}
+		>
+			{/* First row, members surrounded by two empty cells */}
+			<div></div>
+			{members.map((member, index) => (
+				<div key={member._id}>
+					<div key={member._id} className="user-card">
+						<img
+							src={member.avatar}
+							alt={`${member.name}'s avatar`}
+							className="avatar"
+							style={{
+								width: "50px",
+								height: "50px",
+								borderRadius: "50%",
+							}}
+						/>
+						<p>{member.name}</p>
+					</div>
 				</div>
 			))}
+			<div></div>
+
+			{/* The rest of the table */}
+			{schedule.map((row, index) => {
+				const fullRow = ["title", ...row, "delete"];
+				return fullRow.map((cell, jindex) => {
+					console.log(fullRow);
+					if (typeof cell === "string") {
+						if (cell === "title") {
+							return (
+								<div key={index + "title"}>
+									Cycle #{index + 1}
+								</div>
+							);
+						} else {
+							return (
+								<button
+									className="delete"
+									onClick={() => removeRow(index)}
+								>
+									{trash}
+								</button>
+							);
+						}
+					} else {
+						return <div>{cell ? "✔️" : "❌"}</div>;
+					}
+				});
+			})}
 		</div>
 	);
 };
